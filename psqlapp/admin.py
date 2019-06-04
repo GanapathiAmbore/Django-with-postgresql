2355c09f6fb3625ff09974fcabd6d7ba19748cd6
Ganapathi <ganapathiambore@gmail.com>
2019-06-04 19:04:34 +0530

    Displaying image in Adminpanel

--- psqlapp/admin.py
+++ psqlapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from psqlapp.models import Actor
 
 class Actoradmin(admin.ModelAdmin):
-    list_display = ['Name','address','date','age']
+    list_display = ['Name','address','date','age','image_tag']
 
 
 admin.site.register(Actor,Actoradmin)
--- psqlapp/models.py
+++ psqlapp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-
+from django.utils.safestring import mark_safe
 class Actor(models.Model):
     Name=models.CharField(max_length=50)
     address=models.TextField()
     date=models.DateTimeField(auto_now_add=True)
     age=models.PositiveIntegerField()
+    image=models.ImageField(null=True,blank=True)
 
     def __str__(self):
         return self.Name
+
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" style="width: 100px; height:100px;" />' % self.image.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
+
